b6c88ae3131f2ef4e06852f8a9cdd3ca86184c3d
Brennan Vincent <devopsbrennanvincent@gmail.com>
2025-06-07 21:21:28 -0700

    adding api gateway steps

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -116,3 +116,43 @@ resource "aws_lambda_function" "view_counter_function" {
   source_code_hash = filebase64sha256("view_counter.zip")
 
 }
+
+resource "aws_apigatewayv2_api" "view_counter_api" {
+  name = "view-counter-api"
+  protocol_type = "HTTP"
+
+  cors_configuration {
+    allow_headers = ["*"]
+    allow_methods = ["GET"]
+    allow_origins = ["https://${var.resume_domain_name}"]
+    expose_headers = []
+    max_age = 3600
+  }
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id = aws_apigatewayv2_api.view_counter_api.id
+  integration_type = "AWS_PROXY"
+  integration_uri = aws_lambda_function.view_counter_function.invoke_arn
+  integration_method = "POST"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "view_counter_route" {
+  api_id = aws_apigatewayv2_api.view_counter_api.id
+  route_key = "GET /views"
+  target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "prod_stage" {
+  api_id = aws_apigatewayv2_api.view_counter_api.id
+  name = "prod"
+}
+
+resource "aws_lambda_permission" "allow_api" {
+  statement_id = "AllowExecutionFromAPIGateway"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.view_counter_function.function_name
+  principal = "apigateway.amazonaws.com"
+  source_arn = "${aws_apigatewayv2_api.view_counter_api.execution_arn}/*/*"
+}
